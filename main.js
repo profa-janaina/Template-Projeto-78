54f4f0545f854d04a0af8ddb02c50d121e6a255e
profa-janaina <111704392+profa-janaina@users.noreply.github.com>
2023-09-06 16:33:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var images = ["imagem1.jpg",]; //adicione as imagens
+
+var names = ["nome",]; //adicione os nomes dos membros da família
+
+var i = 0;
+function update() {
+    i++;
+    var numberOfFamilyMemberInArray = ; //atualize o tamanho da array    
+    if(i>numberOfFamilyMemberInArray){
+        i=0;
+    }
+
+     //crie uma array para guardar as imagens
+    //crie uma array para guardar os nomes
+
+    document.getElementById("familyMemberImage") //complete o código para adicionar as imagens
+    document.getElementById("familyMemberName")  //complete o código para adicionar os nomes
+}
